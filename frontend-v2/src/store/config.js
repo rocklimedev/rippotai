d225055f9230d4edf244abc3c0fa2da13410afea
Dhruvermafz <vermadhruv09112002@gmail.com>
2026-03-24 15:13:09 +0530

    added the api

--- frontend-v2/src/store/config.js
+++ frontend-v2/src/store/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://rippotai.onrender.com/api"
+    ? "https://api.rippotaiarchitecture.com/api"
     : "http://localhost:5000/api";
