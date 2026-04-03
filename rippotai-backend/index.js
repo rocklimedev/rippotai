5e8a81e71220ce20c48fdf126982387076f9c83c
Dhruv Verma <vermadhruv09112002@gmail.com>
2026-04-03 15:44:42 +0530

    did the

--- rippotai-backend/index.js
+++ rippotai-backend/index.js
@@ -30,6 +30,7 @@ const corsOptions = {
     "https://rippotaiarchitecture.com",
     "https://rippotai.vercel.app",
     "https://cmtradingco.com",
+    "https://www.cmtradingco.com",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
